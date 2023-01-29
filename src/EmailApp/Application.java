426ec98ed94c67decbb49e9de19abf49d1ec7bad
sarkakoparka <sarapanek22@gmail.com>
2023-01-29 12:30:01 +0100

    First commit

---
+++ src/EmailApp/Application.java
@@ -0,0 +1,9 @@
+package EmailApp;
+
+public class Application {
+    public static void main (String[] args){
+        Email testEmail = new Email("Sara", "Panek");
+
+
+    }
+}
---
+++ src/EmailApp/Email.java
@@ -0,0 +1,35 @@
+package EmailApp;
+        /*Your application should do the following:
+     Generate an email with the following syntax: firstname.lastname@department.company.com
+     Determine the department (sales, development, accounting), if none leave blank
+     Generate a random String for a password
+     Have set methods to change the password, set the mailbox capacity, and define an alternate
+    email address
+     Have get methods to display the name, email, and mailbox capacity*/
+
+public class Email {
+   private String firstName;
+    private String lastName;
+   private String department;
+   private String pswd;
+   private int mailboxCapacity;
+   String alternativeEmail;
+
+   Email (String firstName,String lastName){
+       this.firstName=firstName;
+       this.lastName=lastName;
+   }
+
+   // generate e-mail with following syntax
+
+    // determine the dep
+
+    //generate random password
+    //set method to change the password
+    //set mailbox capacity
+    //alternative e-mail
+    //get to display the name, email and capacity of mailbox
+
+
+
+}
