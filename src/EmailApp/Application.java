d639d4a8dfaec109d0e5363e8438cf7c30cb4a07
sarkakoparka <sarapanek22@gmail.com>
2023-05-06 09:06:13 +0200

    email syntax, random password generator method

--- src/EmailApp/Application.java
+++ src/EmailApp/Application.java
@@ -2,9 +2,7 @@ package EmailApp;
 
 public class Application {
     public static void main (String[] args){
-        Email testEmail = new Email("Sara", "Panek");
-
-
+        Email testEmail = new Email("Sara", "Panek", "Dev");
 
     }
 }
--- src/EmailApp/Email.java
+++ src/EmailApp/Email.java
@@ -7,38 +7,86 @@ package EmailApp;
     email address
      Have get methods to display the name, email, and mailbox capacity*/
 
+import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Email {
-   private String firstName;
+    private String firstName;
     private String lastName;
-   private String department;
-   private String pswd;
-   private int mailboxCapacity;
-   private String alternativeEmail;
+    private String department;
+    private String password;
+    private int actualPasswordLength = 10;
+    private int mailboxCapacity;
+    private String alternativeEmail;
+    private String email;
 
-   public Email (String firstName,String lastName){
-       this.firstName=firstName;
-       this.lastName=lastName;
-       this.department=department();
-       System.out.println("E-mail created: "+ this.firstName + "."+ this.lastName + "@" +department+".company.com");
-   }
-   String department (){
-       System.out.println("Type for dep:/n1 - sales /n2 - dev /n3 - marketing");
-       Scanner in = new Scanner(System.in);
-       int dep = in.nextInt();
-       if (dep == 1){return "sales";}
-       else if (dep ==2 ){ return "dev"; }
-       else if (dep == 3){ return "marketing";}
-       else {return "none";}
+    // constructor
+    public Email(String firstName, String lastName, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        System.out.println("E-mail created: " + this.firstName + "." + this.lastName + ".company.com");
 
-   }
+    // call a method asking for department -> return department
+    this.department = setDepartment();
+    System.out.println("Department: "+this.department);
+    //call a method that returns generated random password
+        this.password = randomPassword(actualPasswordLength);
+        System.out.println("Your password is: "+ password);
+    // e-mail syntax
+       email = firstName.toLowerCase() + lastName.toLowerCase() + "@" +  department.toLowerCase() + "." + "company.com";
+        System.out.println(" Your email: " + email);
+    }
+// ask 4 dep.
+    private String setDepartment() {
+        System.out.println("Type for dep:/n1 - sales /n2 - developers /n3 - marketing");
+        Scanner in = new Scanner(System.in);
+        int dep = in.nextInt();
+        if (dep == 1) {
+            return "sales";
+        } else if (dep == 2) {
+            return "developers";
+        } else if (dep == 3) {
+            return "marketing";
+        } else {
+            return "nothing to show";
+        }
+    }
 
-   // generate e-mail with following syntax
+    // generate random password
+    private String randomPassword( int length ){
+        String passwd = "abcdefghijklmnoprstwxyzABCDEFGHIJKLMNOPRSTUWXYZ1234567890!@#$%^&*()";
+        char[] password = new char [length];
+        for (int i=0; i< length; i++){
+            int random = (int) (Math.random()*passwd.length());
+            password[i]= passwd.charAt(random);
+        }
+        return new String (password);
+    }
 
-    // determine the dep
+}
 
     //generate random password
+    /*private class passwordGenerator{
+
+    String lowerCase = "abcdefghijklmnoprstuwxyzźż";
+    String upperCase= "ABCDEFGHIJKLMNOPRSTUWXYZŹŻ";
+    String numeros = "0123456789";
+    String specialCharacter = "<>!@#$%^&*(){}[]/|,.";
+
+    String pswdSyntax=specialCharacter+lowerCase+upperCase+numeros;
+    int lenght ;
+    char [] passwd = new char [lenght];
+    Random random = new Random();
+    for (int i = 0 ; i< lenght; i++) {
+        passwd[i]=pswdSyntax.charAt(random.nextInt(pswdSyntax)));
+
+        }
+    System.out.println("Generated password: "+new String(passwd));
+
+       } */
+
     //set method to change the password
     //set mailbox capacity
     //alternative e-mail
@@ -46,4 +94,4 @@ public class Email {
 
 
 
-}
+
